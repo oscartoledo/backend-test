8f4e2e07e1474231a83ef31271420105a8e66fa4
Oscar Toledo <osky.toledo@gmail.com>
2017-04-05 10:43:20 -0400

    Event Detail

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def date_time_format (date)
+    date.strftime '%b %d @ %H:%M'
+  end
+
+  def date_format (date)
+    date.strftime '%b %d'
+  end
+
+  def time_format (date)
+    date.strftime '%H:%M'
+  end
+
+  def price_format (number)
+    number_to_currency number, precision: 2
+  end
+
 end
